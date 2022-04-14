b04accbfdda34b82ab4c7fe58247445803ba1bca
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2022-04-14 10:34:23 +1000

    ADDED ConsumeAction

---
+++ src/game/actions/ConsumeAction.java
@@ -0,0 +1,32 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.Status;
+
+public class ConsumeAction extends Action{
+    
+    private String hotKey;
+    private Status statusProvided;
+
+    public ConsumeAction() {
+
+    }
+
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    
+}
