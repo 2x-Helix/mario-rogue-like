2d1b4cfe765c85d65b111e9fdb1d506cf262f937
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2022-04-14 10:16:40 +1000

    UPDATED Coin class
    ADDED methods of this class I can think of now

--- src/game/items/Coin.java
+++ src/game/items/Coin.java
@@ -3,23 +3,38 @@ package game.items;
 import edu.monash.fit2099.engine.items.Item;
 
 /**
- * TODO: Implement this class
+ * A class for the currency in this game
  */
 public class Coin extends Item{
 
-    private static final String NAME = "Coin";
-    private static final char DISPLAY_CHAR = '$';
-    private static final boolean PORTABLE = true;
+    /**
+     * public to skip all the getter
+     * static as the values are the same for all object in this class
+     * final as the values don't change
+     */
+    public static final String NAME = "Coin";
+    public static final char DISPLAY_CHAR = '$';
+    public static final boolean PORTABLE = true;
 
+    /**
+     * Value of the coin holds
+     */
     private Integer value;
 
     /**
-     * 
-     * @param value
+     * Public Constructor for this class
+     * @param value Value of the coin holds
      */
     public Coin(Integer value) {
         super(NAME, DISPLAY_CHAR, PORTABLE);
         this.value = value;
+    }
+
+    /**
+     * @return The value of this coin
+     */
+    public Integer getValue() {
+        return this.value;
     }
 
 }
