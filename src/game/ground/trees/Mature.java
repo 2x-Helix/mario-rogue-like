d5bcb74bbbc61f7a9b17ea817d2e98cfbae3cd57
msie0003 <msie0003@student.monash.edu>
2022-04-18 16:11:10 +1000

    ADDED Sapling - grow(), grows to Mature - spawn, 10% chance to spawn $20 coin

--- src/game/ground/trees/Mature.java
+++ src/game/ground/trees/Mature.java
@@ -1,6 +1,4 @@
 package game.ground.trees;
 
-import edu.monash.fit2099.engine.positions.Ground;
-
-public final class Mature extends Ground {
+public final class Mature extends Tree {
 }
--- src/game/ground/trees/Sapling.java
+++ src/game/ground/trees/Sapling.java
@@ -1,6 +1,41 @@
 package game.ground.trees;
 
-import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.Utils;
+import game.items.Coin;
 
-public final class Sapling extends Ground {
+/**
+ * Sapling has 10% chance to spawn a coin and takes 10 turns to grow.
+ * @author Matthew Siegenthaler
+ * 1.0
+ */
+public final class Sapling extends Tree {
+    // Constructor
+    public Sapling() {
+        super('t', 10);
+    }
+
+    /**
+     * Grows into Mature
+     * @param location Location of the Tree.
+     */
+    @Override
+    public void grow(Location location) {
+        location.setGround(new Mature());
+    }
+
+    /**
+     * Sapling has 10% chance to spawn $20 coin.
+     * @param location Location of the Tree.
+     * @return boolean if Sapling spawns coin.
+     */
+    @Override
+    public boolean spawn(Location location) {
+        if (Utils.nextChance() <= 10) {
+            location.addItem(new Coin(20));
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
