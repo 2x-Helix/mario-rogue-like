d0a72c79c1e27b410606e8f4e4cd822361053486
msie0003 <msie0003@student.monash.edu>
2022-04-18 18:36:49 +1000

    ADDED Utils.nextInt() method - Generate random number between [low, high) UPDATED nextChance() to use .nextInt()

--- src/game/Utils.java
+++ src/game/Utils.java
@@ -4,17 +4,30 @@ import java.util.Random;
 
 /**
  * Utilities class containing random number generator
+ * @author Matthew Siegenthaler
+ * @version 1.1
  */
 public class Utils {
+    /**
+     * Generates a new random number in bounds
+     * @param low Lower integer bound of generation (inclusive)
+     * @param high Upper integer bound of generation (exclusive)
+     * @return Randomly generated integer in bounds
+     */
+    public static int nextInt(int low, int high) throws IllegalArgumentException {
+        Random r = new Random();
+        if (low <= high){
+            return (r.nextInt(high - low) + low);
+        } else {
+            throw new IllegalArgumentException("Low must be less than High.");
+        }
+    }
+
     /**
      * Pseudo-randomly generates an integer for a chance roll between 1-100
      * @return Generate integer for chance roll between 1-100
      */
     public static int nextChance(){
-        Random r = new Random();
-        int low = 1;
-        int high = 100;
-        return (r.nextInt(high - low) + low);
+        return nextInt(1, 101);
     }
 }
-
