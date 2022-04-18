3068e11a6cbbd9354bffd57ee8e4058fe450a78d
msie0003 <msie0003@student.monash.edu>
2022-04-18 18:08:29 +1000

    UPDATED Mature - grow(), Try to grow Sprout on adjacent fertile tiles every 5 turns - spawn, 15% chance to spawn Koopa - every tick, 20% chance to wither to dirt. ADDED GroundCapabilities enum, FERTILE capability UPDATED Dirt constructor to give tile FERTILE capability ADDED Utils.nextInt(), generated random integer in range UPDATED javadoc

--- src/game/Utils.java
+++ src/game/Utils.java
@@ -9,14 +9,26 @@ import java.util.Random;
  */
 public class Utils {
     /**
+     * Generates a new random number in bounds
+     * @param low Lower integer bound of generation
+     * @param high Upper integer bound of generation
+     * @return Randomly generated integer in bounds
+     */
+    public static int nextInt(int low, int high) throws IllegalArgumentException {
+        Random r = new Random();
+        if (low < high){
+            return (r.nextInt(high - low) + low);
+        } else {
+            throw new IllegalArgumentException("Low must be less than High.");
+        }
+    }
+
+    /**
      * Pseudo-randomly generates an integer for a chance roll between 1-100
      * @return Generate integer for chance roll between 1-100
      */
     public static int nextChance(){
-        Random r = new Random();
-        int low = 1;
-        int high = 100;
-        return (r.nextInt(high - low) + low);
+        return nextInt(0, 100);
     }
 }
 
