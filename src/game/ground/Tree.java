f0a3632b384d8909e51094ddfcc6afa74ca26a90
msie0003 <msie0003@student.monash.edu>
2022-04-18 12:12:12 +1000

    ADDED abstract class Tree extends Ground - grows after a set amount of turns (growthCounter - abstract method grow() - Tree's growth behaviour after specified number of turns has passed - abstract method spawn() - Spawning behaviour of tree each turn.
    ADDED Utils javadoc

--- src/game/ground/Tree.java
+++ src/game/ground/Tree.java
@@ -1,14 +1,89 @@
 package game.ground;
 
+import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
 
-public class Tree extends Ground {
+/**
+ * Tree is a Ground that grows after a set period of time and
+ * has spawning behaviour each turn.
+ * @author Matthew Siegenthaler
+ * @version 1.0
+ */
+public abstract class Tree extends Ground {
+    private Integer growthCounter;
 
     /**
      * Constructor.
      *
      */
-    public Tree() {
-        super('+');
+    public Tree(char displayChar, Integer growthCounter) {
+        super(displayChar);
+
+        if (!(setGrowthCounter(growthCounter))) {
+            throw new IllegalArgumentException("growthCounter must be between > 0");
+        }
+    }
+
+    /* Setters */
+
+    /**
+     * Setter method for growth counter.
+     * @param newGrowthCounter Number of turns (>0) Tree requires to grow for.
+     * @return Boolean if growthCounter was set.
+     */
+    public Boolean setGrowthCounter(Integer newGrowthCounter) {
+        if (newGrowthCounter > 0) {
+            this.growthCounter = newGrowthCounter;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /* Methods */
+
+    /**
+     * Tree will reduce it growth counter, try to spawn, and grow if end of growth cycle.
+     * @param location The location of the Ground
+     */
+    @Override
+    public void tick(Location location) {
+        this.growthCounter--;
+        this.spawn(location);
+
+        if (growthCounter == 0)
+            this.grow(location);
+    }
+
+    /**
+     * Called once growth counter of Tree is 0.
+     * @param location Location of the Tree.
+     */
+    public abstract void grow(Location location);
+
+    /**
+     * Tree spawning behaviour each turn
+     * @param location Location of the Tree.
+     */
+    public abstract void spawn(Location location);
+
+    /**
+     * Actors cannot enter tree
+     * @param actor the Actor to check
+     * @return Boolean false (Actors can never enter)
+     */
+    @Override
+    public boolean canActorEnter(Actor actor) {
+        return false;
+    }
+
+    /**
+     * Tree block throw items
+     * @return boolean true (always blocks)
+     */
+    @Override
+    public boolean blocksThrownObjects() {
+        return true;
     }
 }
