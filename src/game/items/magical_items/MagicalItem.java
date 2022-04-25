c2d2ec7710c60a39b21f0b86fb13839209b6fcd4
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2022-04-26 00:47:16 +1000

    UPDATED magical items to provide status

--- src/game/items/magical_items/MagicalItem.java
+++ src/game/items/magical_items/MagicalItem.java
@@ -1,13 +1,17 @@
 package game.items.magical_items;
 
+import java.util.ArrayList;
+
 import edu.monash.fit2099.engine.items.Item;
+import game.Status;
+
 /**
  * TODO: Implement this class
  */
 public abstract class MagicalItem extends Item{
     
     /**
-     * 
+     * Protected contructor of this class
      * @param name
      * @param displayChar
      * @param portable
--- src/game/items/magical_items/SuperMushroom.java
+++ src/game/items/magical_items/SuperMushroom.java
@@ -1,8 +1,8 @@
 package game.items.magical_items;
 
-/**
- * TODO: Implement this class
- */
+import edu.monash.fit2099.engine.items.DropItemAction;
+import game.Status;
+import game.actions.ConsumeAction;
 public class SuperMushroom extends MagicalItem{
     
     private static final String NAME = "Super Mushroom";
@@ -14,6 +14,17 @@ public class SuperMushroom extends MagicalItem{
      */
     public SuperMushroom() {
         super(NAME, DISPLAY_CHAR, PORTABLE);
+        this.addCapability(Status.INCREASED_MAX_HP);
+        this.addCapability(Status.EASY_JUMP);
+        this.addAction(new ConsumeAction(this));
+        this.addAction(new DropItemAction(this));
+    }
+
+    /**
+     * Create and return an action to consume this item
+     */
+    public ConsumeAction getConsumeAction() {
+        return new ConsumeAction(this);
     }
 
 }
