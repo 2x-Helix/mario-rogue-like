ca87a4d72723b4e422504a417418eb23ac9499c0
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2022-04-25 22:13:09 +1000

    UPDATED Status to includes status from magical items
    ADDED INCREASED_MAX_HP
    ADDED EASY_JUMP
    ADDED HIGHER_GROUND
    ADDED IMMUNITY
    ADDED INSTA_KILL

--- src/game/Status.java
+++ src/game/Status.java
@@ -5,6 +5,11 @@ package game;
  * It is also useful to give a `state` to abilities or actions that can be attached-detached.
  */
 public enum Status {
-    HOSTILE_TO_ENEMY, // use this status to be considered hostile towards enemy (e.g., to be attacked by enemy)
-    TALL, // use this status to tell that current instance has "grown".
+    HOSTILE_TO_ENEMY,   // use this status to be considered hostile towards enemy (e.g., to be attacked by enemy)
+    TALL,               // use this status to tell that current instance has "grown".
+    INCREASED_MAX_HP,   // indicates actor have 50 more max hp
+    EASY_JUMP,          // indicates actor can jump with 100% success rate
+    HIGHER_GROUND,      // indicates actor can just walk to higher ground, destroy higher ground into dirt when walk, destroyed ground drop $5 
+    IMMUNITY,           // indicates actor takes 0 damage when attacked by enemies
+    INSTA_KILL          // indicates actor instantly kill enemies if the attack is successful
 }
