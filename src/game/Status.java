cfc7e0675bf07e5af7b4a287254e899118d5277c
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2022-04-25 22:13:09 +1000

    UPDATED Status to includes status from magical items
    ADDED INCREASED_MAX_HP
    ADDED EASY_JUMP
    ADDED HIGHER_GROUND
    ADDED IMMUNITY
    ADDED INSTA_KILL

--- src/game/Status.java
+++ src/game/Status.java
@@ -9,9 +9,7 @@ public enum Status {
     TALL,               // use this status to tell that current instance has "grown".
     INCREASED_MAX_HP,   // indicates actor have 50 more max hp
     EASY_JUMP,          // indicates actor can jump with 100% success rate
-    HIGHER_GROUND,      // indicates actor can just walk to higher ground, destroy higher ground into dirt when walk, destroyed ground drop $5
+    HIGHER_GROUND,      // indicates actor can just walk to higher ground, destroy higher ground into dirt when walk, destroyed ground drop $5 
     IMMUNITY,           // indicates actor takes 0 damage when attacked by enemies
-    INSTA_KILL,         // indicates actor instantly kill enemies if the attack is successful
-    INDESTRUCTIBLE,      // indicates actor cannot be removed when hp is less than or equal to 0
-    DORMANT             // indicates actor cannot attack, follow or wander around
+    INSTA_KILL          // indicates actor instantly kill enemies if the attack is successful
 }
