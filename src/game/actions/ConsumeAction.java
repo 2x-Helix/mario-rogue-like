40108e4c3f300747f15b1aa1985f7a290a5d19c7
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2022-04-26 00:49:52 +1000

    UPDATED ConsumeAction

--- src/game/actions/ConsumeAction.java
+++ src/game/actions/ConsumeAction.java
@@ -2,31 +2,51 @@ package game.actions;
 
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
-import game.Status;
+import game.items.magical_items.PowerStar;
 
 public class ConsumeAction extends Action{
     
-    private String hotKey;
-    private Status statusProvided;
+    /**
+     * The item to be consumed
+     */
+    private Item item;
 
-    public ConsumeAction() {
-
+    /**
+     * Public constructor for this class
+     * @param item the item to be consumed
+     */
+    public ConsumeAction(Item item) {
+        this.item = item;
     }
 
-
+    /**
+     * Actor consumes the items
+     * 
+     * @param actor The actor performing the action.
+	 * @param map The map the actor is on.
+	 * @return a description of what happened that can be displayed to the user.
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
-        // TODO Auto-generated method stub
-        return null;
+        for (Enum<?> capability : item.capabilitiesList()) {
+            actor.addCapability(capability);
+        }
+        if (!(this.item instanceof PowerStar)) {
+            actor.removeItemFromInventory(this.item);
+        }
+        return this.menuDescription(actor);
     }
 
+    /**
+	 * Returns a descriptive string
+	 * @param actor The actor performing the action.
+	 * @return the text we put on the menu
+	 */
     @Override
     public String menuDescription(Actor actor) {
-        // TODO Auto-generated method stub
-        return null;
+        return actor + " consumes " + item;
     }
 
-
-    
 }
