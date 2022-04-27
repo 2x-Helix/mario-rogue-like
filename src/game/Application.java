76bb0ffe473fd6b6b178a324bf45326057eca943
jamespractice <jhuy0014@student.monash.edu>
2022-04-27 19:36:03 +1000

    Added Action: SpeakAction, Weapon: Wrench, Updated Application to automatically put Toad onto  the GameMap every new game.

--- src/game/Application.java
+++ src/game/Application.java
@@ -10,6 +10,7 @@ import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.World;
 import game.actors.Goomba;
 import game.actors.Player;
+import game.actors.Toad;
 import game.ground.Dirt;
 import game.ground.Floor;
 import game.ground.Tree;
@@ -25,7 +26,7 @@ public class Application {
 
 			World world = new World(new Display());
 
-			FancyGroundFactory groundFactory = new FancyGroundFactory(new Dirt(), new Wall(), new Floor(), new Tree());
+			FancyGroundFactory groundFactory = new FancyGroundFactory(new Dirt(), new Wall(), new Floor(), new Sprout());
 
 			List<String> map = Arrays.asList(
 				"..........................................##..........+.........................",
@@ -53,6 +54,9 @@ public class Application {
 
 			Actor mario = new Player("Player", 'm', 100);
 			world.addPlayer(mario, gameMap.at(42, 10));
+
+			Actor toad = new Toad();
+			world.addPlayer(toad, gameMap.at(43, 9));
 
 			// FIXME: the Goomba should be generated from the Tree
 			gameMap.at(35, 10).addActor(new Goomba());
