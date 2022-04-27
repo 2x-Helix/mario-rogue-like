a6d9c2316abd3f63cc7572fd7daedfcd31c5b825
msie0003 <msie0003@student.monash.edu>
2022-04-28 09:50:53 +1000

    Revert "Added Jumpable interface, Implemented JumpActorAction, updated SuperMushroom to contain certain capabilities."
    This reverts commit 17fa91ef

---
+++ src/game/ground/Tree.java
@@ -0,0 +1,14 @@
+package game.ground;
+
+import edu.monash.fit2099.engine.positions.Ground;
+
+public abstract class Tree extends Ground {
+
+    /**
+     * Constructor.
+     *
+     */
+    public Tree() {
+        super('+');
+    }
+}
