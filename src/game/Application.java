0f87936e41ffc2feecdc820f4d3a60b72b693455
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2022-04-28 15:39:01 +1000

    MOVED Goomba to game.actors.enemies

--- src/game/Application.java
+++ src/game/Application.java
@@ -8,8 +8,8 @@ import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.FancyGroundFactory;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.World;
-import game.actors.enemies.Goomba;
 import game.actors.friendly.Player;
+import game.actors.friendly.Toad;
 import game.ground.Dirt;
 import game.ground.Floor;
 import game.ground.Wall;
@@ -56,8 +56,7 @@ public class Application {
 			Actor mario = new Player("Player", 'm', 100);
 			world.addPlayer(mario, gameMap.at(42, 10));
 
-			// FIXME: the Goomba should be generated from the Tree
-			gameMap.at(35, 10).addActor(new Goomba());
+			gameMap.at(42, 8).addActor(new Toad());
 
 			// Spawn these 2 items at the spawn point of Mario
 			gameMap.locationOf(mario).addItem(new SuperMushroom());
