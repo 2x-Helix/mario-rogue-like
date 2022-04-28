2bd7336568fda8f10e255853a80f9c29575fc894
jamespractice <jhuy0014@student.monash.edu>
2022-04-28 10:53:23 +1000

    Modified Floor to restrict enemies.

--- src/game/ground/Floor.java
+++ src/game/ground/Floor.java
@@ -1,6 +1,8 @@
 package game.ground;
 
+import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
+import game.actors.Enemy;
 
 /**
  * A class that represents the floor inside a building.
@@ -8,5 +10,14 @@ import edu.monash.fit2099.engine.positions.Ground;
 public class Floor extends Ground {
 	public Floor() {
 		super('_');
+	}
+
+	// Restricts enemies from entering this ground
+	@Override
+	public boolean canActorEnter(Actor actor) {
+		if(actor instanceof Enemy){
+			return false;
+		}
+		return true;
 	}
 }
