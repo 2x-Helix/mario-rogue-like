ae277f888753cfe0c94a61064a8e828478fb6819
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2022-04-28 10:22:35 +1000

    UPDATED Jumpable into HighGround
    All ground type that is Jumpable now extends HighGround

--- src/game/ground/Wall.java
+++ src/game/ground/Wall.java
@@ -1,14 +1,28 @@
 package game.ground;
 
+import edu.monash.fit2099.engine.actors.Actor;
+import game.Utils;
+import game.status.Status;
+
 public class Wall extends HighGround {
 
 	public Wall() {
-		super('#');
+		super('#', 80, 20);
 	}
 	
 	@Override
 	public boolean blocksThrownObjects() {
 		return true;
+	}
+
+	@Override
+    public boolean onJump(Actor actor) {
+		if (actor.hasCapability(Status.EASY_JUMP) || Utils.nextChance() <= this.successThreshhold) {
+			return true;
+		} else {
+			actor.hurt(this.fallDamage);
+			return false;
+		}
 	}
 
 }
