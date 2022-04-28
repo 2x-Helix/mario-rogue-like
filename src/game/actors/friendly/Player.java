d06d159c1820e1845a912f3228e80f8c0338444e
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2022-04-28 14:38:42 +1000

    UPDATED Friendly to be hostile to enemy

--- src/game/actors/friendly/Player.java
+++ src/game/actors/friendly/Player.java
@@ -25,7 +25,6 @@ public class Player extends Actor  {
 	 */
 	public Player(String name, char displayChar, int hitPoints) {
 		super(name, displayChar, hitPoints);
-		this.addCapability(Status.HOSTILE_TO_ENEMY);
 	}
 
 	@Override
