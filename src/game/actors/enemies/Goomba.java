52c1f70635ad948a666a5cc2910aa924019c2bb7
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2022-04-28 11:35:39 +1000

    ADDED Koopa; MOVED Goomba

--- src/game/actors/enemies/Goomba.java
+++ src/game/actors/enemies/Goomba.java
@@ -1,4 +1,4 @@
-package game.actors;
+package game.actors.enemies;
 
 
 import java.util.HashMap;
---
+++ src/game/actors/enemies/Koopa.java
@@ -0,0 +1,22 @@
+package game.actors.enemies;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.GameMap;
+
+public class Koopa extends Actor{
+
+
+    public Koopa() {
+        super("Koopa", 'K', 100);
+    }
+
+    @Override
+    public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
