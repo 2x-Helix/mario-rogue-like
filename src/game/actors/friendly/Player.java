a5e805dcb9ec10df02a9f6629f22c7d4bed8967e
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2022-04-28 10:23:25 +1000

    FIXED playTurn's stink

--- src/game/actors/friendly/Player.java
+++ src/game/actors/friendly/Player.java
@@ -5,9 +5,7 @@ import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.displays.Menu;
-import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
-import game.actions.ConsumeAction;
 import game.status.Status;
 import game.status.StatusManager;
 
@@ -33,30 +31,13 @@ public class Player extends Actor  {
 	@Override
 	public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
 
-		StatusManager statusManager = StatusManager.getStatusManager();
-		
-		statusManager.tick();	// tick for statuses
+		StatusManager.getStatusManager().tick();	// tick for statuses
 
-		// TODO: fix this stinky poopo
-		for (Item item : this.getInventory()) {
-			if (item.getClass().getSuperclass().getSimpleName() == "MagicalItem") {
-				actions.add(new ConsumeAction(item));
-			}
-		}
-		
 		// Handle multi-turn Actions
 		if (lastAction.getNextAction() != null)
 			return lastAction.getNextAction();
 
-		if (this.capabilitiesList().contains(Status.IMMUNITY)) {
-			String cout = "Mario consumes Power Star - " + statusManager.getStatusDuration(this, Status.IMMUNITY);
-			if (statusManager.getStatusDuration(this, Status.IMMUNITY) > 1) {
-				cout += " turns remaining";
-			} else {
-				cout += " turn remaining";
-			}
-			System.out.println(cout);
-		}
+		System.out.print(this.statusDescription());
 
 		// return/print the console menu
 		return menu.showMenu(this, actions, display);
@@ -65,6 +46,25 @@ public class Player extends Actor  {
 	@Override
 	public char getDisplayChar(){
 		return this.hasCapability(Status.TALL) ? Character.toUpperCase(super.getDisplayChar()): super.getDisplayChar();
+	}
+
+	/**
+	 * @return a description of statuses this player has
+	 */
+	private String statusDescription() {
+
+		StatusManager statusManager = StatusManager.getStatusManager();
+
+		if (this.capabilitiesList().contains(Status.IMMUNITY)) {
+			String cout = "Mario consumes Power Star - " + statusManager.getStatusDuration(this, Status.IMMUNITY);
+			if (statusManager.getStatusDuration(this, Status.IMMUNITY) > 1) {
+				cout += " turns  \n";
+			} else {
+				cout += " turn remaining \n";
+			}
+			return cout;
+		}
+		return "";
 	}
 
 }
