3386dbe4de98562778378f1142eab446e41d5880
jamespractice <jhuy0014@student.monash.edu>
2022-04-30 15:30:17 +1000

    Implemented FollowBehaviour in Enemy abstract class, and added javadoc.

--- src/game/actors/Enemy.java
+++ src/game/actors/Enemy.java
@@ -1,11 +1,27 @@
 package game.actors;
 
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actions.DoNothingAction;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.actions.AttackAction;
+import game.behaviours.AttackBehaviour;
+import game.behaviours.Behaviour;
+import game.behaviours.FollowBehaviour;
+import game.behaviours.WanderBehaviour;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * @TODO: Implment this class
+ * This class is an abstract class for the Enemy actors
+ * @author James Huynh
+ * @version 1.0
  */
 public abstract class Enemy extends Actor{
+    private final Map<Integer, Behaviour> behaviours = new HashMap<>(); // priority, behaviour
 
     /**
      *
@@ -15,6 +31,32 @@ public abstract class Enemy extends Actor{
      */
     protected Enemy(String name, char displayChar, int hitPoints) {
         super(name, displayChar, hitPoints);
+        this.behaviours.put(0, new AttackBehaviour());
+        this.behaviours.put(10, new WanderBehaviour());
+
     }
-    
+
+    /**
+     * Figure out what to do next.
+     * @see Actor#playTurn(ActionList, Action, GameMap, Display)
+     */
+    @Override
+    public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+        for(Behaviour behaviour : behaviours.values()) {
+            Action action = behaviour.getAction(this, map);
+            if (action != null)
+                return action;
+        }
+        return new DoNothingAction();
+    }
+
+    @Override
+    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+        ActionList list = super.allowableActions(otherActor, direction, map);
+        list.add(new AttackAction(this, direction));
+        // After AttackAction is called on this current actor, add FollowBehaviour to this actor's list
+        // of behaviours
+        behaviours.put(1, new FollowBehaviour(otherActor));
+        return list;
+    }
 }
