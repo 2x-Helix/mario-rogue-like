9620b0b4295a8cce82ba58519f5e868805c5dcf0
jamespractice <jhuy0014@student.monash.edu>
2022-05-01 22:10:10 +1000

    Updated Goomba class, added SuicideAction

---
+++ src/game/actions/SuicideAction.java
@@ -0,0 +1,18 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+
+public class SuicideAction extends Action {
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        map.removeActor(actor);
+        return menuDescription(actor);
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return actor + " suicides";
+    }
+}
