7c5f7a07c2b0470e410bf6be66aa83d9cb0ceecb
msie0003 <msie0003@student.monash.edu>
2022-05-01 14:52:17 +1000

    REMOVAL of WeaponItem in game.items.weapon_items UPDATED Wrench to extend WeaponItem in engine.weapons

--- src/game/items/weapon_items/Wrench.java
+++ src/game/items/weapon_items/Wrench.java
@@ -1,19 +1,24 @@
 package game.items.weapon_items;
 
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+
 /**
- * TODO: Implement this class
+ * Wrench weapon used to smash Koopa shells.
+ * @author ChunKau Mok, Matthew Siegenthaler
+ * @version 2.0
  */
 public class Wrench extends WeaponItem {
     
     private static final String NAME = "Wrench";
-    private static final char DISPLAY_CHAR = 'W';   // Peter: not sure 
-    private static final boolean PORTABLE = false;
+    private static final char DISPLAY_CHAR = 'W';
+    private static final int DAMAGE = 50;
+    private static final String VERB = "smashes";
+    private static final int HIT_RATE = 80;
 
     /**
-     * 
+     * Constructor.
      */
     public Wrench() {
-        super(NAME, DISPLAY_CHAR, PORTABLE);
+        super(NAME, DISPLAY_CHAR, DAMAGE, VERB, HIT_RATE);
     }
-
 }
