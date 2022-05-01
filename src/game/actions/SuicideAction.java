fb66b644a0c8d423de776037088f7435f8f3431c
jamespractice <jhuy0014@student.monash.edu>
2022-05-01 22:10:10 +1000

    Updated Goomba class, added SuicideAction

--- src/game/actions/SuicideAction.java
+++ src/game/actions/SuicideAction.java
@@ -4,11 +4,6 @@ import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
 
-/**
- * Class implementing the suicide Action.
- * @author James Huynh
- * @version 1.0
- */
 public class SuicideAction extends Action {
     @Override
     public String execute(Actor actor, GameMap map) {
--- src/game/actors/enemy/Goomba.java
+++ src/game/actors/enemy/Goomba.java
@@ -1,31 +1,56 @@
 package game.actors;
 
-
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.actions.DoNothingAction;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
+import game.Utils;
 import game.actions.AttackAction;
 import game.Status;
+import game.actions.SuicideAction;
 import game.behaviours.Behaviour;
-import game.behaviours.WanderBehaviour;
+import game.behaviours.FollowBehaviour;
 
-import java.util.HashMap;
-import java.util.Map;
 /**
  * A little fungus guy.
  */
-public class Goomba extends Actor {
-	private final Map<Integer, Behaviour> behaviours = new HashMap<>(); // priority, behaviour
+public class Goomba extends Enemy {
 
-	/**
-	 * Constructor.
-	 */
+	// Constructor
 	public Goomba() {
-		super("Goomba", 'g', 50);
-		this.behaviours.put(10, new WanderBehaviour());
+		super("Goomba", 'g', 20);
+	}
+
+	@Override
+	protected IntrinsicWeapon getIntrinsicWeapon() {
+		return new IntrinsicWeapon(10, "kicks");
+	}
+
+	@Override
+	public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+		// Chance to self-destruct, 10%
+		if(Utils.nextChance() <= 10) {
+			return new SuicideAction();
+		}
+
+		// checks if lastAction was AttackAction
+		if(lastAction instanceof AttackAction) {
+			// check if behaviours contains AttackBehaviour already
+			if(!behaviours.containsKey(1)) {
+				// adds FollowBehaviour, targeting the actor the current actor called AttackAction on
+				this.behaviours.put(1, new FollowBehaviour(((AttackAction) lastAction).getTarget()));
+			}
+		}
+
+		for(Behaviour behaviour : behaviours.values()) {
+			Action action = behaviour.getAction(this, map);
+			if (action != null)
+				return action;
+		}
+		return new DoNothingAction();
 	}
 
 	/**
@@ -39,26 +64,11 @@ public class Goomba extends Actor {
 	 */
 	@Override
 	public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
-		ActionList actions = new ActionList();
+		ActionList list = new ActionList();
 		// it can be attacked only by the HOSTILE opponent, and this action will not attack the HOSTILE enemy back.
 		if(otherActor.hasCapability(Status.HOSTILE_TO_ENEMY)) {
-			actions.add(new AttackAction(this,direction));
+			list.add(new AttackAction(this,direction));
 		}
-		return actions;
+		return list;
 	}
-
-	/**
-	 * Figure out what to do next.
-	 * @see Actor#playTurn(ActionList, Action, GameMap, Display)
-	 */
-	@Override
-	public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
-		for(Behaviour Behaviour : behaviours.values()) {
-			Action action = Behaviour.getAction(this, map);
-			if (action != null)
-				return action;
-		}
-		return new DoNothingAction();
-	}
-
 }
