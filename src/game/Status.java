47728f4325293cb872adad35ef1d68b01bb01bc7
jamespractice <jhuy0014@student.monash.edu>
2022-05-01 23:01:29 +1000

    Updated SmashShellAction, Removed StationaryBehaviour, Updated Status, Updated Koopa and AttackAction.

--- src/game/Status.java
+++ src/game/Status.java
@@ -5,8 +5,13 @@ package game;
  * It is also useful to give a `state` to abilities or actions that can be attached-detached.
  */
 public enum Status {
-    HOSTILE_TO_ENEMY, // use this status to be considered hostile towards enemy (e.g., to be attacked by enemy)
-    TALL, // use this status to tell that current instance has "grown".
-    INVINCIBLE, // use this to tell current instance is invincible (powerstar effect)
-    RESET, // Use this to indicate a Resettable has been reset
+    HOSTILE_TO_ENEMY,   // use this status to be considered hostile towards enemy (e.g., to be attacked by enemy)
+    TALL,               // use this status to tell that current instance has "grown".
+    INCREASED_MAX_HP,   // indicates actor have 50 more max hp
+    EASY_JUMP,          // indicates actor can jump with 100% success rate
+    HIGHER_GROUND,      // indicates actor can just walk to higher ground, destroy higher ground into dirt when walk, destroyed ground drop $5
+    IMMUNITY,           // indicates actor takes 0 damage when attacked by enemies
+    INSTA_KILL,         // indicates actor instantly kill enemies if the attack is successful
+    INDESTRUCTIBLE,      // indicates actor cannot be removed when hp is less than or equal to 0
+    DORMANT             // indicates actor cannot attack, follow or wander around
 }
--- src/game/actions/AttackAction.java
+++ src/game/actions/AttackAction.java
@@ -5,16 +5,14 @@ import java.util.Random;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
-import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.weapons.Weapon;
 import game.Utils;
 import game.status.Status;
 
 /**
  * Special Action for attacking other Actors.
- * modified by: James Huynh
- * @version 2.0
  */
 public class AttackAction extends Action {
 
@@ -30,7 +28,7 @@ public class AttackAction extends Action {
 
 	/**
 	 * Constructor.
-	 * 
+	 *
 	 * @param target the Actor to attack
 	 */
 	public AttackAction(Actor target, String direction) {
@@ -48,20 +46,20 @@ public class AttackAction extends Action {
 		}
 
 		int damage;
-		if (actor.hasCapability(Status.INSTA_KILL)) {			// Mario consumes POWERSTAR and can insta kill enemies(target)
+		if (actor.hasCapability(Status.INSTA_KILL)) {            // Mario consumes POWERSTAR and can insta kill enemies(target)
 			damage = Integer.MAX_VALUE;
-		} else if (target.hasCapability(Status.IMMUNITY)) {		// Mario(target) consumes POWERSTAR and have immunity against enemies'(actor) attacks
+		} else if (target.hasCapability(Status.IMMUNITY)) {        // Mario(target) consumes POWERSTAR and have immunity against enemies'(actor) attacks
 			damage = 0;
 		} else {
 			damage = weapon.damage();
 		}
-		
+
 		String result = actor + " " + weapon.verb() + " " + target + " for " + damage + " damage.";
 		target.hurt(damage);
 
 		if (!target.isConscious()) {
 			// check if target does not have indestructible status
-			if (!target.hasCapability(Status.INDESTRUCTIBLE)){
+			if (!target.hasCapability(Status.INDESTRUCTIBLE)) {
 				ActionList dropActions = new ActionList();
 				// drop all items
 				for (Item item : target.getInventory())
@@ -71,6 +69,10 @@ public class AttackAction extends Action {
 				// remove actor
 				map.removeActor(target);
 				result += System.lineSeparator() + target + " is killed.";
+			}
+			else {
+				// if target is indestructible but not conscious, then it is dormant
+				target.addCapability(Status.DORMANT);
 			}
 		}
 		return result;
