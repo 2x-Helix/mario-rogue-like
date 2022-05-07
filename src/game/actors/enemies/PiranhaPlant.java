08a4b6e4941780c8e50732423b8f5940f47403a7
msie0003 <msie0003@student.monash.edu>
2022-05-07 16:29:44 +1000

    UPDATED PiranhaPlant() constructor for PiranhaPlant - Remove arguments

--- src/game/actors/enemies/PiranhaPlant.java
+++ src/game/actors/enemies/PiranhaPlant.java
@@ -8,11 +8,8 @@ public class PiranhaPlant extends Enemy {
 
     /**
      * Constructor.
-     * @param name        the name of the Actor
-     * @param displayChar the character that will represent the Actor in the display
-     * @param hitPoints   the Actor's starting hit points
      */
-    public PiranhaPlant(String name, char displayChar, int hitPoints) {
+    public PiranhaPlant() {
         super(NAME, DISPLAY_CHAR, START_HEALTH);
     }
 }
