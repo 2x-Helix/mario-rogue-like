9855d70378bf6fcdf2ce61a2b5a590a054f0226c
msie0003 <msie0003@student.monash.edu>
2022-05-12 09:53:49 +1000

    IMPLEMENTED Resettable interface for PiranhaPlant - Increase max hp by 50 - Fully heal plant

--- src/game/actors/enemies/PiranhaPlant.java
+++ src/game/actors/enemies/PiranhaPlant.java
@@ -1,15 +1,26 @@
 package game.actors.enemies;
 
-public class PiranhaPlant extends Enemy {
+public class PiranhaPlant extends Enemy{
     // Constants
     private static final String NAME = "Piranha Plant";
     private static final char DISPLAY_CHAR = 'Y';
     private static final int START_HEALTH = 150;
+    private static final int BONUS_HEALTH = 50;
 
     /**
      * Constructor.
      */
     public PiranhaPlant() {
         super(NAME, DISPLAY_CHAR, START_HEALTH);
+        registerResettable();
+    }
+
+    /**
+     * Piranha plant to fully heal and receive bonus health.
+     */
+    @Override
+    public void resetInstance() {
+        this.increaseMaxHp(BONUS_HEALTH);   // Bonus health on reset
+        this.heal(getMaxHp());              // Fully heal
     }
 }
