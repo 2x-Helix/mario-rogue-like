69900f6c2ae2b6aaf69cee424391e691eec1aa32
jamespractice <jhuy0014@student.monash.edu>
2022-05-12 16:12:13 +1000

    Removed EndGameAction and replaced UnlockAction with RescueAction.

--- src/game/actions/RescueAction.java
+++ src/game/actions/RescueAction.java
@@ -4,14 +4,20 @@ import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
 
-public class EndGameAction extends Action {
-    @Override
-    public String execute(Actor actor, GameMap map) {
-        return null;
+public class RescueAction extends Action {
+    private final Actor freed;
+
+    public RescueAction(Actor freed) {
+        this.freed = freed;
     }
 
     @Override
+    public String execute(Actor actor, GameMap map) {
+        map.removeActor(actor);
+        return menuDescription(actor);
+    }
+    @Override
     public String menuDescription(Actor actor) {
-        return null;
+        return "Congratulations," + actor + " has freed " + freed + "!";
     }
 }
--- src/game/actors/friendlies/PrincessPeach.java
+++ src/game/actors/friendlies/PrincessPeach.java
@@ -3,21 +3,29 @@ package game.actors.friendlies;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actions.DoNothingAction;
+import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
-import game.actions.EndGameAction;
+import game.actions.RescueAction;
 import game.status.Status;
 
 public class PrincessPeach extends Friendly {
     protected PrincessPeach() {
         super("Princess Peach", 'P', Integer.MAX_VALUE);
+        this.addCapability(Status.LOCKED);
     }
 
     @Override
     public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
-        if(!this.hasCapability(Status.LOCKED)){
-            return new EndGameAction();
-        }
         return new DoNothingAction();
+    }
+
+    @Override
+    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+        ActionList actions = super.allowableActions(otherActor, direction, map);  // Get parents action list
+        if(this.hasCapability(Status.LOCKED) && otherActor.hasCapability(Status.HAS_KEY)){
+            actions.add(new RescueAction(this));  // If otherActor has a key, can use UnlockAction on Peach
+        }
+        return actions;
     }
 }
