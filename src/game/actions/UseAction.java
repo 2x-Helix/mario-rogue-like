efb927dbe37c47a19a2799431a70fcdca76e8d01
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2022-05-16 18:27:01 +1000

    ADDED UseAction for equipment to provide to player

---
+++ src/game/actions/UseAction.java
@@ -0,0 +1,22 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+
+/**
+ * Allows players to use equipments
+ */
+public class UseAction extends Action {
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        return null;
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return null;
+    }
+
+}
