a14f399bb9043aa1672585a6e638d689ccafb67c
msie0003 <msie0003@student.monash.edu>
2022-05-20 01:36:28 +1000

    ADDED class KingdomZone extends GameMap - Instantiates Mushroom kingdom gamemap ADDED class LavaZone extends GameMap - Instantiates Lava Zone Gamemap

---
+++ src/game/zones/LavaZone.java
@@ -0,0 +1,34 @@
+package game.zones;
+
+import edu.monash.fit2099.engine.positions.FancyGroundFactory;
+import edu.monash.fit2099.engine.positions.GameMap;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Lava zone map where the player may fight Bowser and rescue Princess Peach.
+ * @author Matthew Siegenthaler
+ * @version 1.0
+ */
+public class LavaZone extends GameMap {
+    private static final List<String> strMap = Arrays.asList(
+        "C......L#############L.......",
+        ".......L#LLLLLLLLLLL#L.......",
+        ".......L#LL.......LL#L.......",
+        ".......L#L.........L#L.......",
+        ".......L#L.........L#L.......",
+        ".......L#LL.......LL#L.......",
+        ".......L#LLLL...LLLL#L.......",
+        ".......L#####___#####L.......",
+        ".......LLLLL#___#LLLLL.......",
+        "...........L#___#L...........",
+        ".............................",
+        ".............................",
+        "............................."
+    );
+
+    public LavaZone(FancyGroundFactory groundFactory) {
+        super(groundFactory, strMap);
+        // Add actors
+    }
+}
